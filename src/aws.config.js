d620d44db234cc1d491ca253b1b55eeb462b1963
21pintoo-singh <superdudepintoo@gmail.com>
2022-07-25 16:57:59 +0530

    auth&validation

---
+++ src/aws.config.js
@@ -0,0 +1,42 @@
+const aws = require('aws-sdk');
+
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
+
+
+let uploadFile = async (file) => {
+    return new Promise(function (resolve, reject) {
+        // this function will upload file to aws and return the link
+        let s3 = new aws.S3({
+            apiVersion: '2006-03-01'
+        }); // we will be using the s3 service of aws
+
+        var uploadParams = {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket", //HERE
+            Key: "group39/userProfile/file_" + new Date().getTime() + file.originalname, //HERE 
+            Body: file.buffer
+        }
+
+        s3.upload(uploadParams, function (err, data) {
+            if (err) {
+                return reject(err)
+            }
+            // console.log(data)
+            console.log("file uploaded succesfully")
+            return resolve(data.Location)
+        })
+
+        // let data= await s3.upload( uploadParams)
+        // if( data) return data.Location
+        // else return "there is an error"
+
+    })
+}
+
+module.exports = {
+    uploadFile
+}
---
+++ src/utility/validation.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose")
+const moment = require("moment");
+
+
+let isEmptyObject = function(body){
+      if(!body) return true
+      if(Object.keys(body).length==0) return true;
+      return false;
+}
+
+let isEmptyVar = function(value){
+      if(typeof value === 'undefined' || value === null) return true;
+      if(value.trim().length===0) return true;
+      return false;
+}
+
+let isValidPhone= function (number){
+      let phoneRegex = /^[6-9]\d{9}$/;
+      return phoneRegex.test(number);
+}
+
+let isValidEmail = function(email){
+      let emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      return emailRegex.test(email)
+}
+
+let isValidPassword=function(password){
+      let passwordRegex=/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/
+      return passwordRegex.test(password)
+}
+
+let isValidDateFormat=function(date){
+      let dateFormatRegex=/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
+      return dateFormatRegex.test(date)
+}
+
+let isValidDate = function(date){
+      return moment(date).isValid()
+}
+
+let isValidObjectId=function(ObjectId){
+      return mongoose.isValidObjectId(ObjectId)
+}
+
+let isEmptyFile=(file) => {
+      if (!file || file.length==0) return true
+      return false
+}
+
+const acceptFileType = (file, ...Types) => {
+      return Types.indexOf(file.mimetype) !==-1 ? true : false
+}
+
+const isPincodeValid = function (value){
+      return /^[1-9]{1}[0-9]{5}$/.test(value);
+}
+
+let isValidJSONstr=(json) =>{
+      try{
+            return JSON.parse(json)
+      } catch (_) {
+            return false
+      }
+}
+
+let checkArrContent = (array, ...isContentArray) =>{
+      let count = 0
+      array.forEach(e =>{
+            if (!isContentArray.includes(e)) count++
+      });
+      return count == 0 ? true : false
+}
+
+module.exports = {
+      isEmptyObject,
+      isEmptyVar,
+      isValidEmail,
+      isValidPhone,
+      isValidPassword,
+      isValidObjectId,
+      isValidDateFormat,
+      isValidDate,
+      isEmptyFile,
+      acceptFileType,
+      isValidJSONstr,
+      isPincodeValid,
+      checkArrContent
+  }
