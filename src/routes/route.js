58abf0a83e90ccf1ab2ac946195852540295f0a6
Nishant8826 <rnishant609@gmail.com>
2022-07-25 21:29:55 +0530

    create

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,51 +1,24 @@
-const userModel = require('../models/userModels')
-const vfy = require('../utility/validation')
-const jwt = require('jsonwebtoken')
-const authentication = (req, res, next) => {
-    try{
-        let token = req.headers.authorization
-        if(vfy.isEmptyVar(token)) return res.status(400).send({ status: false, Message: "⚠️ The token must be required in 'Bearer'" })
-        // console.log(token)
-        // split and get the token only 🤯🤯
-        token = token.split(' ')[1] // get the 1 index value
-        jwt.verify(token,'secret',function(err,decode){
-            if(err){ 
-                return res.status(401).send({ status: false, Message: err.message })
-            }else{
-                // console.log(decode)
-                req.tokenData = decode;
-                next()
-            }
-        })
-    }catch(_){
-        res.status(500).send({ status: false, Message: _.message })
-    }
-}
+const express=require("express")
+const router=express.Router()
+
+const userController=require("../controllers/userController")
 
 
-const authorization_user = async (req, res, next) => {
-    //👉 get user id fron params
-    const userId = req.params.userId
+router.post("/register",userController.createUser)
 
-    // 👉 get user id from token
-    const token = req.tokenData
+router.get("/user/:userId/profile",userController.getUserById)
 
-    // 👉 check valid object id
-    if(!vfy.isValidObjectId(userId)) return res.status(400).send({ status: false, Message: "Invalid user ID!" })
-
-    // check the user exist in db
-    const user = await userModel.findById(userId)
-    if(!user) return res.status(404).send({ status: false, Message: "⚠️ No user found!" })
-
-    // auth Z 🔐
-    if(userId !== token.userId) return res.status(401).send({ status: false, Message: "🔒 Unauthorized user!" })
-
-    next()
-}
+// router.put("/user/:userId/profile",userController.updateUserProfile)
 
 
 
-module.exports = {
-    authentication,
-    authorization_user
-}
+
+
+
+
+
+
+
+
+
+module.exports=router
--- src/utility/validation.js
+++ src/utility/validation.js
@@ -1,88 +1,23 @@
-const mongoose = require("mongoose")
-const moment = require("moment");
+const mongoose = require("mongoose");
+const ObjectId=mongoose.Schema.Types.ObjectId
 
+function isValid(value){
+    if(typeof value==="undefined" || typeof value===null)return false;
+    if(typeof value ==="string" && value.trim().length ==0)return false;
+    return true
+};
 
-let isEmptyObject = function(body){
-      if(!body) return true
-      if(Object.keys(body).length==0) return true;
-      return false;
+function isValidRequestBody(value){
+    return Object.keys(value)>0
 }
 
-let isEmptyVar = function(value){
-      if(typeof value === 'undefined' || value === null) return true;
-      if(value.trim().length===0) return true;
-      return false;
-}
+const isValidObjectId = function (objectId) {
+    return ObjectId.isValid(objectId)
+};
 
-let isValidPhone= function (number){
-      let phoneRegex = /^[6-9]\d{9}$/;
-      return phoneRegex.test(number);
-}
+const validEmail=/^[a-zA-Z0-1]+[@][a-z]{3,6}[.][a-z]{2,5}$/
+const validPhone=/^[6789][0-9]{9}$/
+const validFName=/^[a-zA-Z]+$/ 
+const validLName=/^[a-zA-Z]+$/
 
-let isValidEmail = function(email){
-      let emailRegex=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-      return emailRegex.test(email)
-}
-
-let isValidPassword=function(password){
-      let passwordRegex=/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/
-      return passwordRegex.test(password)
-}
-
-let isValidDateFormat=function(date){
-      let dateFormatRegex=/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/
-      return dateFormatRegex.test(date)
-}
-
-let isValidDate = function(date){
-      return moment(date).isValid()
-}
-
-let isValidObjectId=function(ObjectId){
-      return mongoose.isValidObjectId(ObjectId)
-}
-
-let isEmptyFile=(file) => {
-      if (!file || file.length==0) return true
-      return false
-}
-
-const acceptFileType = (file, ...Types) => {
-      return Types.indexOf(file.mimetype) !==-1 ? true : false
-}
-
-const isPincodeValid = function (value){
-      return /^[1-9]{1}[0-9]{5}$/.test(value);
-}
-
-let isValidJSONstr=(json) =>{
-      try{
-            return JSON.parse(json)
-      } catch (_) {
-            return false
-      }
-}
-
-let checkArrContent = (array, ...isContentArray) =>{
-      let count = 0
-      array.forEach(e =>{
-            if (!isContentArray.includes(e)) count++
-      });
-      return count == 0 ? true : false
-}
-
-module.exports = {
-      isEmptyObject,
-      isEmptyVar,
-      isValidEmail,
-      isValidPhone,
-      isValidPassword,
-      isValidObjectId,
-      isValidDateFormat,
-      isValidDate,
-      isEmptyFile,
-      acceptFileType,
-      isValidJSONstr,
-      isPincodeValid,
-      checkArrContent
-  }
+module.exports={isValid,isValidRequestBody,validEmail,validPhone,isValidObjectId,validFName,validLName}
