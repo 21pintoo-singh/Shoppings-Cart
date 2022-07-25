f2267f8355605defd69d072e97427416aaf3a0f0
Nishant8826 <rnishant609@gmail.com>
2022-07-25 22:50:53 +0530

    modified

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,32 +1,41 @@
 const userModel=require("../models/userModels")
 const bcrypt=require("bcrypt")
 const validator=require("../utility/validation")
-const aws=require("../utility/aws.config")
+const aws=require("../utility/awsconfig")
+const mongoose=require("mongoose")
+const ObjectId=mongoose.Types.ObjectId
+
+const validEmail=/^[a-zA-Z0-1]+[@][a-z]{3,6}[.][a-z]{2,5}$/
+const validPhone=/^[6789][0-9]{9}$/
+const validFName=/^[a-zA-Z]+$/ 
+const validLName=/^[a-zA-Z]+$/
 
 const createUser=async (req,res)=>{
     try{
         let files = req.files;
         const requestBody=req.body
-        console.log(requestBody)
-        if(!validator.isValidRequestBody(requestBody))return res.status(400).send({ status: false, message: 'Please provide user details' })
+        // console.log(requestBody)
+        if(!validator.isValidBody(requestBody))return res.status(400).send({ status: false, message: 'Please provide user details' })
 
-        const { fname, lname, phone, email, password, address } = requestBody// Object destructing
-        console.log(fname)
-        console.log(requestBody)
-        
+        let { fname, lname, phone, email, password, address } = requestBody// Object destructing
+        // console.log(fname)
+        // console.log(requestBody)
+        let ad=JSON.parse(address)
+        requestBody.address=ad
+        // console.log(address)
 //----------------------------------Validation starts--------------------------------
         //fname
         if(!validator.isValid(fname))return res.status(400).send({ status: false, message: `fname is required` });
-        if(!validator.validFName.test(fname))return res.status(400).send({ status: false, message: `fname is in invalid Format` });
+        if(!validFName.test(fname))return res.status(400).send({ status: false, message: `fname is in invalid Format` });
 
         //lname
         if (!validator.isValid(lname)) return res.status(400).send({ status: false, message: `lname is required ` });
-        if(!validator.validLName.test(lname))return res.status(400).send({ status: false, message: `lname is in invalid format ` });
+        if(!validLName.test(lname))return res.status(400).send({ status: false, message: `lname is in invalid format ` });
 
         //email
         if (!validator.isValid(email)) return res.status(400).send({ status: false, message: `Email is required` })
         email = email.trim().toLowerCase()
-        if (!(validator.validEmail.test(email))) return res.status(400).send({ status: false, message: `Email should be a valid email address ` })
+        if (!(validEmail.test(email))) return res.status(400).send({ status: false, message: `Email should be a valid email address ` })
         const isEmailAlreadyUsed = await userModel.findOne({ email }); 
         if (isEmailAlreadyUsed) return res.status(400).send({ status: false, message: `${email} email address is already registered` })
 
@@ -37,7 +46,7 @@ const createUser=async (req,res)=>{
         //phone
         if (!validator.isValid(phone)) return res.status(400).send({ status: false, message: 'phone no is required' });
         phone = phone.trim()
-        if (!(validator.validNumber.test(phone))) return res.status(400).send({ status: false, message: `Please fill a valid phone number` })
+        if (!(validPhone.test(phone))) return res.status(400).send({ status: false, message: `Please fill a valid phone number` })
         const isPhoneAlreadyUsed = await userModel.findOne({ phone });
         if (isPhoneAlreadyUsed) return res.status(400).send({ status: false, message: `${phone} phone number is already registered` })
 
@@ -46,20 +55,21 @@ const createUser=async (req,res)=>{
         password = password.trim()
         if (!(password.length > 7 && password.length < 16)) return res.status(400).send({ status: false, message: "password should  between 8 and 15 characters" })
        
-
         //address
-        if(!validator.isValidRequestBody(address))return res.status(400).send({ status: false, message: `Address is required` })
+
+        if(!validator.isValidBody(address))return res.status(400).send({ status: false, message: `Address is required` })
         if(!typeof address=="object")return res.status(400).send({ status: false, message: `Address is in Invalid Format` })
-        address=JSON.parse(address)
-        if (!address.shipping || (address.shipping && (!address.shipping.street || !address.shipping.city || !address.shipping.pincode))) return res.status(400).send({ status: false, message: 'Shipping address is required' })
-        if (!address.billing || (address.billing && (!address.billing.street || !address.billing.city || !address.billing.pincode))) return res.status(400).send({ status: false, message: 'Billing address is required' })
+        
+        // if (!address.shipping || (address.shipping && (!address.shipping.street || !address.shipping.city || !address.shipping.pincode))) return res.status(400).send({ status: false, message: 'Shipping address is required' })
+        // if (!address.billing || (address.billing && (!address.billing.street || !address.billing.city || !address.billing.pincode))) return res.status(400).send({ status: false, message: 'Billing address is required' })
         
 // ---------------------------------Validation ends-------------------------------
         const salt =await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
-        const userData = { fname, lname, phone, email,profileImage: profileImage,  password:hashedPassword, address };
-
-        const newUser = await userModel.create(userData);
+        requestBody.password=hashedPassword
+        requestBody.profileImage=profileImage
+        
+        const newUser = await userModel.create(requestBody);
         return res.status(201).send({ status: true, message: ` User created successfully`, data: newUser });
     }catch(error){
         return res.status(500).send({ status: false, message: error.message });
@@ -69,8 +79,8 @@ const createUser=async (req,res)=>{
 const getUserById=async (req,res)=>{
     try{
         const userId=req.params.userId
-        if(!validator.isValidObjectId(userId)) return res.status(400).send({ status: false, message: `${userId} is not a valid userId` })
-        const user=await findOne({_id:userId})
+        if(!ObjectId(userId)) return res.status(400).send({ status: false, message: `${userId} is not a valid userId` })
+        const user=await userModel.findOne({_id:userId})
         if(!user)return res.status(404).send({status:false,message:"UserId Not found"})
         return res.status(200).send({ status: true, message: 'User profile details', data: user })
     }catch(error){
--- src/utility/validation.js
+++ src/utility/validation.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const ObjectId=mongoose.Schema.Types.ObjectId
+
 
 function isValid(value){
     if(typeof value==="undefined" || typeof value===null)return false;
@@ -7,17 +7,14 @@ function isValid(value){
     return true
 };
 
-function isValidRequestBody(value){
-    return Object.keys(value)>0
+function isValidBody(value){
+    return Object.keys(value).length >0
 }
 
-const isValidObjectId = function (objectId) {
-    return ObjectId.isValid(objectId)
-};
+// const isValidObjectId = function (objectId) {
+//     return ObjectId.isValid(objectId)
+// };
 
-const validEmail=/^[a-zA-Z0-1]+[@][a-z]{3,6}[.][a-z]{2,5}$/
-const validPhone=/^[6789][0-9]{9}$/
-const validFName=/^[a-zA-Z]+$/ 
-const validLName=/^[a-zA-Z]+$/
 
-module.exports={isValid,isValidRequestBody,validEmail,validPhone,isValidObjectId,validFName,validLName}
+
+module.exports={isValid,isValidBody}
