ba627f929f03da9861665fe4052e44b4aaa3b7fb
ajit kerle <ajitkerle70@gmail.com>
2022-08-04 19:12:33 +0530

    changed

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -3,14 +3,14 @@ const ObjectId=mongoose.Schema.Types.ObjectId
 
 const orderSchema=new mongoose.Schema(
     {
-        userId: {type: ObjectId, ref:"User", required:true},
+        userId: {type: ObjectId, ref:"User"},
         items: [{
-          productId: {type:ObjectId, ref:'Product',required:true},
-          quantity: {type:Number,required:true }
+          productId: {type:ObjectId, ref:'Product'},
+          quantity: {type:Number}
         }],
-        totalPrice: {type:Number, required:true},
-        totalItems: {type:Number,  required:true},
-        totalQuantity: {type:Number,  required:true},
+        totalPrice: {type:Number},
+        totalItems: {type:Number},
+        totalQuantity: {type:Number},
         cancellable: {type:Boolean, default: true},
         status: {type:String, default: 'pending'},// enum[pending, completed, cancled]},
         deletedAt: {type:Date}, // when the document is deleted}, 
